4ebdc357184ad522b61d4280751615620c455623
Adool <adelsaid284@gmail.com>
2025-06-12 22:51:23 +0300

    create model for onboarding  screen

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_wodget_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_wodget_view.dart
@@ -1,5 +1,5 @@
-import 'package:dalel/core/utils/app_assets.dart';
 import 'package:dalel/core/utils/app_text_style.dart';
+import 'package:dalel/features/on_boarding/data/models/on_boarding_model.dart';
 import 'package:dalel/features/on_boarding/presentation/views/widgets/custom_smooth_page_indecator.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +12,7 @@ class OnBoardingWidgetBody extends StatelessWidget {
       height: 500,
 
       child: PageView.builder(
+        itemCount: onBoardingData.length,
         physics: const BouncingScrollPhysics(),
         controller: _controller,
         itemBuilder: (context, index) {
@@ -20,9 +21,9 @@ class OnBoardingWidgetBody extends StatelessWidget {
               Container(
                 height: 290,
                 width: 343,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage(Assets.assetsImagesOnBoarding1),
+                    image: AssetImage(onBoardingData[index].imagePath),
                     fit: BoxFit.fill,
                   ),
                 ),
@@ -32,7 +33,7 @@ class OnBoardingWidgetBody extends StatelessWidget {
               const SizedBox(height: 32),
 
               Text(
-                "Exploure The History  with Dalel in smart way ",
+                onBoardingData[index].title,
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
                 style: AppTextStyle.poppins500Style24.copyWith(
@@ -42,8 +43,8 @@ class OnBoardingWidgetBody extends StatelessWidget {
               ),
               const SizedBox(height: 12),
 
-              const Text(
-                "test run project",
+              Text(
+                onBoardingData[index].subTitle,
                 style: AppTextStyle.poppins300Style16,
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
